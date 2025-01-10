7ba32534251bc25d420895c6701feff8bdf8318d
Andrea Ciccarello <58073848+AndreaCicca@users.noreply.github.com>
2025-01-10 19:35:35 +0100

    aggiunta sezione about

--- src/app/view/about/about.component.ts
+++ src/app/view/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { marked } from 'marked';
 
 @Component({
@@ -7,10 +7,29 @@ import { marked } from 'marked';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
   constructor() {
-    this.renderMarkdown('# About\n\n');
+
+  }
+  ngOnInit(): void {
+    this.renderMarkdown(`# Informazioni a proposito del progetto.
+
+Il progetto affronta il problema di semplificare e ottimizzare l’accesso a un ampio archivio di documenti, spesso caratterizzati da una struttura non intuitiva e difficili da consultare in modo efficace. L’obiettivo è sviluppare un sistema intelligente che consenta agli utenti di interagire con il contenuto di questi documenti in maniera fluida, utilizzando il linguaggio naturale per porre domande e ricevere come risposta l'elenco dei documenti maggiormente attinenti all'argomento ricercato.
+
+Questo sistema dovrà organizzare, processare e indicizzare i documenti, sfruttando tecnologie avanzate di intelligenza artificiale, come modelli di linguaggio e database vettoriali, per comprendere e restituire le informazioni più rilevanti. In tal modo, si mira a risolvere le difficoltà legate alla ricerca manuale e al recupero di informazioni, rendendo l’intero processo più efficiente e accessibile.
+
+## Progettazione dell'architettura
+
+L'architettura del progetto prevede i seguenti elementi infrastrutturali e le loro relazioni:
+- Un Database vettoriale per memorizzare gli embeddings dei documenti e i relativi metadati: in questo caso si è scelto di utilizzare il database Qdrant. Il vector DB ha il compito di facilitare la ricerca e recupero dei contenuti rilevanti in base alla vicinanza semantica con la query dell'utente.
+- Uno o più Modelli di linguaggio LLM per interpretare le domande degli utenti, generare embeddings per le query e costruire risposte basate sui documenti recuperati.
+- Un sistema ad agenti che possa gestire l’invocazione dei diversi strumenti utilizzati per effettuare le query e formattare l’output.
+- Una API REST per servire il sistema verso l’esterno.
+- Una interfaccia web che permetta all’utente di interagire con il sistema.
+
+Il vector DB Qdrant, il sistema ad agenti, l’API e pagina web sono istanziate sulla stessa macchina, mentre i modelli LLM sono serviti in cloud (DeepSeek) o da una macchina esterna (Llama 3.2, qwq).
+`);
   }
 
   public renderMarkdown(markdown: string): void {
